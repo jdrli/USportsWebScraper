16f00444212777c6212e9fef3296808e43f91d53
jdrli <jackyli9875@gmail.com>
2024-07-12 10:57:17 -0400

    Updated offensive stat updating methods.

--- webscrape.py
+++ webscrape.py
@@ -6,80 +6,49 @@ import time
 
 # driver.get("https://universitysport.prestosports.com/sports/mvball/" + str(year-1) + "-" + str(year)[-2:] + "/teams")
 
-# This function updates the full stats table with every team in it for the given year. 
-def UpdateTeams(year):
-    driver = webdriver.Chrome()
-    driver.get("https://universitysport.prestosports.com/sports/mvball/2023-24/teams")
-    statsdoc = BeautifulSoup(driver.page_source, 'html.parser')
-
-    print(statsdoc.prettify())
-    driver.quit
-
-# UpdateTeams(2024)
-
-def UpdatePlayers(year):
+def PlayerUpdateAtk(year):
     url = "https://universitysport.prestosports.com/sports/mvball/" + str(year-1) + "-" + str(year)[-2:] + "/players?sort=&view=&pos=p&r=0"
     stats = requests.get(url)
-    statsdoc = BeautifulSoup(stats.text, 'html.parser')
+    statsdoc = BeautifulSoup(stats.text, "html.parser")
+    tables = statsdoc.find_all("table")
 
-    tables = statsdoc.find_all('table')
-
-    # atk_data = []
-    # for table in tables:
-    #     rows = table.find_all('tr')
-    #     header_data = []
-    #     for row in rows:
-    #         cols = row.find_all(['td', 'th'])
-    #         processed_cols = []
-    #         for i, col in enumerate(cols):
-    #             text = col.text.strip()
-    #             if i == 1:  # Apply replace operations only on the first column
-    #                 text = text.replace('  ', '').replace('\n', ' ').replace('\r', '')
-    #             processed_cols.append(text)
-    #         header_data.append(processed_cols)
-    #     atk_data.append(header_data)
     atk_data = []
     for table in tables:
-        rows = table.find_all('tr')
+        rows = table.find_all("tr")
         header_data = []
         for row in rows:
-            cols = row.find_all(['td', 'th'])
+            cols = row.find_all(["td", "th"])
             cols = [col.text.strip() for col in cols]
             header_data.append(cols)
         atk_data.append(header_data)
-
 
     headers = atk_data[0][0]
     atk_df = pd.DataFrame(atk_data[0][1:], columns=headers)
     atk_df = atk_df.drop(columns = atk_df.columns[:1])
 
-    atk_df.iloc[:, 0] = atk_df.iloc[:, 0].str.replace('  ', '').str.replace('\n', ' ').str.replace('\r', '').str.strip()
+    atk_df.iloc[:, 0] = atk_df.iloc[:, 0].str.replace("  ", "").str.replace("\n", " ").str.replace("\r", "").str.strip()
 
-    print(atk_df)
+    # print(atk_df)
     print("Player statistics updated for " + str(year-1) + "-" + str(year)[-2:] + "!")
     return(atk_df)
 
-    print(df)
-
-# pd.set_option('display.max_columns', None)
-# pd.set_option('display.max_colwidth', None)
-UpdatePlayers(2024)
+PlayerUpdateAtk(2024)
 
 # def UpdateTeams(year):
 #     url = "https://universitysport.prestosports.com/sports/mvball/" + str(year-1) + "-" + str(year)[-2:] + "/teams"
 #     stats = requests.get(url)
-#     statsdoc = BeautifulSoup(stats.text, 'html.parser')
-#     table_rows = statsdoc.find_all('tr')
+#     statsdoc = BeautifulSoup(stats.text, "html.parser")
+#     table_rows = statsdoc.find_all("tr")
     
-#     headers = [header.get_text() for header in table_rows[0].find_all('th')]
+#     headers = [header.get_text() for header in table_rows[0].find_all("th")]
 
 #     data = []
 #     for row in table_rows[1:]:
 #         team_data = []
-#         cols = row.find_all(['td', 'th'])
+#         cols = row.find_all(["td", "th"])
 #         cols = [col.text.strip() for col in cols]
 #         team_data.append(cols)
-#         cells = row.find_all('td')
+#         cells = row.find_all("td")
 #         if len(cells) > 0:
 #             row_data = [cell.get_text(strip=True) for cell in cells]
 #             data.append(row_data)
@@ -88,7 +57,7 @@ UpdatePlayers(2024)
     
 #     # data = []
 #     # for row in table_rows[1:]:  # Skip the header row
-#     #     cells = row.find_all('td')
+#     #     cells = row.find_all("td")
 #     #     if len(cells) > 0:  # Ensure the row is not empty
 #     #         row_data = [cell.get_text(strip=True) for cell in cells]
 #     #         data.append(row_data)
@@ -103,18 +72,18 @@ UpdatePlayers(2024)
 
 # UpdateTeams(2024)
 
-def UpdateOffensive(year):
+def TeamUpdateAtk(year):
     url = "https://universitysport.prestosports.com/sports/mvball/" + str(year-1) + "-" + str(year)[-2:] + "/teams"
     stats = requests.get(url)
-    statsdoc = BeautifulSoup(stats.text, 'html.parser')
-    tables = statsdoc.find_all('table')
+    statsdoc = BeautifulSoup(stats.text, "html.parser")
+    tables = statsdoc.find_all("table")
 
     atk_data = []
     for table in tables:
-        rows = table.find_all('tr')
+        rows = table.find_all("tr")
         team_data = []
         for row in rows:
-            cols = row.find_all(['td', 'th'])
+            cols = row.find_all(["td", "th"])
             cols = [col.text.strip() for col in cols]
             team_data.append(cols)
         atk_data.append(team_data)
@@ -123,13 +92,11 @@ def UpdateOffensive(year):
     atk_df = pd.DataFrame(atk_data[0][1:], columns=headers)
     atk_df = atk_df.drop(columns = atk_df.columns[:1])
 
-    print(atk_df)
-    print("Teams statistics updated for " + str(year-1) + "-" + str(year)[-2:] + "!")
+    # print(atk_df)
+    print("Team offensive statistics updated for " + str(year-1) + "-" + str(year)[-2:] + "!")
     return(atk_df)
-
-# UpdateOffensive(2024)
     
-def UpdateDefensive(year):
-    return year
+TeamUpdateAtk(2024)
 
-# UpdateDefensive(2024)
+# def UpdateDefensive(year):
+#     return year
